8db6ab2a3c6173208afca7328b7b64e07c897852
Nathalie <thom1375@algonquinlive.com>
2019-09-30 16:05:59 -0400

    fixes logic & syntax errors in script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const headerStyle = 'font-size: 24px; font-weight: bold; font-family: sans-serif;'
 const subheaderStyle = 'font-size: 16px; font-weight: bold; font-family: sans-serif;'
 
-console.log('%cQuotes by William Shakespeare", headerStyle)
+console.log(`%c"Quotes by William Shakespeare"`, headerStyle)
 
 const plays = {}
 
-for (const quote of quotes) {
-  if (!plays[quote.play]) {
-    play[quote.play] = []
+for (const thisQuote of quotes) {
+  if (!plays[thisQuote.play]) {
+    plays[thisQuote.play] = [] // if the play doesn't yet exist, create a new array with play title
+    plays[thisQuote.play].push(thisQuote.quote) // CRITICAL! Push this first quote to the new array
   } else {
-    plays[quote.play].push(quote)
+    plays[thisQuote.play].push(thisQuote.quote) // if the play already exists, push the quote to the array
   }
 }
-
 for (const play in plays) {
   console.log('')
-  console.log(`%c${play}`, subheaderStyle)
-  for (let i = 1; i < plays[play].length, i++) {
-    console.log(`${i}. ${plays[play[i]}`)
+  console.log(`%c${play}`, subheaderStyle) // print play title using style from above
+  for (let i = 0; i < plays[play].length; i++) { // has to start at 0 to get first element of quotes array for play
+    console.log(`${i + 1}. ${plays[play][i]}`) // first quote should be 1, not 0, then print matching quotes from array
   }
 }
